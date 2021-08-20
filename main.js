edffc3f8e402589021a08a9b2cd141cf4e80cbcf
Aminu Mukhtar Mado <madoaminu@gmail.com>
2021-08-20 07:36:18 +0100

    added the grid box and a grid slider

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log("we in?")
+let gridCount = 0;
+
+
+
+const gridSizeSlider = document.getElementById("gridSizeSlider");
+console.log(gridSizeSlider);
+let gridSize = gridSizeSlider.value;
+const mainContainer = document.getElementsByClassName("main-container")[0];
+const displayGridSizeValue = document.getElementsByClassName("displayGridSizeValue")[0];
+function displayGridSize(){
+    displayGridSizeValue.textContent = `${gridSizeSlider.value} x ${gridSizeSlider.value}`; 
+}
+displayGridSize();
+gridSizeSlider.addEventListener("input",displayGridSize);
+
+for( let i = 0; i<256;i++){
+    const createDiv = document.createElement("div");
+    mainContainer.appendChild(createDiv);
+    createDiv.classList.add("grid-cell");
+}
