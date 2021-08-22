9dc621ba806e30197eeae5edf610b86232cb1068
Aminu Mukhtar Mado <madoaminu@gmail.com>
2021-08-22 23:09:24 +0100

    refactored the code

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+// Parameters
 const gridSizeSlider = document.getElementById("gridSizeSlider");
 const mainContainer = document.querySelector(".main-container");
 const displayGridSizeValue = document.querySelector(".displayGridSizeValue");
@@ -11,10 +11,10 @@ let grids = gridSizeSlider.value;
 let gridsSquare = '';
 let userColor = 'black';
 let rainbowSwitch =false;
+
+//functions
 function displayGridSize(){
     displayGridSizeValue.textContent = `${gridSizeSlider.value} x ${gridSizeSlider.value}`; 
-    
-    
 }
 
 function setGridSize(){
@@ -27,48 +27,48 @@ function updateGrid(){
     setGridSize();
     mainContainer.innerHTML=''; 
     createDiv();
-    
 }
-
-
 function createDiv(){
-    for( let i = 0; i<gridsSquare;i++){
+    for( let i = 0; i < gridsSquare; i++){
         const createDiv = document.createElement("div");
         mainContainer.appendChild(createDiv);
         createDiv.classList.add("grid-cell");    
-    }
-    let innerGrids = document.querySelectorAll(".grid-cell");
-    console.log(innerGrids);
-    innerGrids.forEach(function(grid){
-        grid.addEventListener('mouseover',function(){
-            if (rainbowSwitch){
-                grid.style.background= `hsl(${Math.random() * 360}, 100%, 50%)`;
-            }else
-            grid.style.background=`${userColor}`;
-        })
-    })
+    };
 }
+
 function clearContainer(){
     let innerGrids = document.querySelectorAll(".grid-cell");
     innerGrids.forEach(function(grid){
-    grid.style.background=`white`;
+        grid.style.background=`white`;
     });
 }
 
 
-
+//EventListeners
 gridSizeSlider.addEventListener("input",displayGridSize);
 gridSizeSlider.addEventListener("change",updateGrid);
 clearBoard.addEventListener("click",clearContainer);
-eraser.addEventListener("click",()=> {userColor="white";
-rainbowSwitch=false});
+eraser.addEventListener("click",()=> {
+    userColor="white";
+    rainbowSwitch=false
+});
 color.addEventListener("click",()=>{
     userColor= "black"
     rainbowSwitch = false;
-} );
-colorPicker.addEventListener("change",()=> {userColor = colorPicker.value
-    rainbowSwitch = false;});
+});
+colorPicker.addEventListener("change",()=> {
+    userColor = colorPicker.value
+    rainbowSwitch = false;
+});
+
 rainbow.addEventListener("click",()=> rainbowSwitch = true);
+mainContainer.addEventListener("mouseover",function(event){
+    if (rainbowSwitch){
+        event.target.style.background = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    }
+    else event.target.style.background = `${userColor}`;
+})
+
 /* Initialization */
 displayGridSize(); //This updates the text of the grid;
 updateGrid(); // This sets the initial grid size;
